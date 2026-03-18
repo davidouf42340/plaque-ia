c899316e11573b384a6935477f6be7568e460a24
davidouf42340 <davidouf42340@gmail.com>
2026-03-18 16:19:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -202,7 +202,7 @@ app.get("/", (req, res) => {
   res.status(200).send("Serveur IA OK");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Serveur IA lancé sur le port " + PORT);
